850cb8898818f7260624569e2109f401c5a2f72b
Ikenna Omekam <iomekam3@gmail.com>
2019-02-22 06:35:51 -0800

    Updates

---
+++ precache-manifest.062eabc819baa3775b881c60a572106d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35380b1841c8c94058fc",
+    "url": "/logisight-web/static/css/main.2533ee16.chunk.css"
+  },
+  {
+    "revision": "35380b1841c8c94058fc",
+    "url": "/logisight-web/static/js/main.35380b18.chunk.js"
+  },
+  {
+    "revision": "8a0f1b70180970f81d68",
+    "url": "/logisight-web/static/js/runtime~main.8a0f1b70.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/logisight-web/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/logisight-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9d32470e9788601e0e07f5f18346c6a3",
+    "url": "/logisight-web/index.html"
+  }
+];
